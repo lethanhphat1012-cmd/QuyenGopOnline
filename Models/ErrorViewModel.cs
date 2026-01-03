7aaedda2fb670246d505e36e236e4fa6d5deff6a
Phat le <lethanhphat10122004@gmail.com>
2026-01-03 14:14:48 +0700

    CHORE: Khởi tạo dự án ASP.NET Core MVC và gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace QuyenGopOnline.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
