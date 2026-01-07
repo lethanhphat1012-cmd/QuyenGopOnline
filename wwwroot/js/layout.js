b7444bef24b345badfbcde62a8acc335c48fe291
Phat le <lethanhphat10122004@gmail.com>
2026-01-07 15:13:29 +0700

    Chinh sua giao dien trang chu

---
+++ wwwroot/js/layout.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector(".custom-navbar");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            navbar.classList.add("scrolled");
+        } else {
+            navbar.classList.remove("scrolled");
+        }
+    });
+
+    // Tự động active menu dựa trên URL hiện tại
+    const currentPath = window.location.pathname;
+    document.querySelectorAll('.nav-link').forEach(link => {
+        if (link.getAttribute('href') === currentPath) {
+            link.classList.add('active');
+        }
+    });
+});
